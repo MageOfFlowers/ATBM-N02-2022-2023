fedd4c273fa2e024c4f6924a5947b8b62606efd7
tai211003 <161297760+tai211003@users.noreply.github.com>
2024-02-29 08:40:14 +0700

    Create CS2.sql

---
+++ CS2.sql
@@ -0,0 +1,39 @@
+alter session set "_ORACLE_SCRIPT"=true;
+
+create role ROLE_GIANGVIEN
+not identified;
+
+grant ROLE_NHANVIE to ROLE_GIANGVIEN;
+
+create function xem_phan_cong_chinh_minh_function(p_schema varchar2, p_obj varchar2)
+return varchar2
+as
+user varchar2(100);
+begin
+user := sys_context('userenv', 'session_user');
+return 'MAGV = ' || user;
+end;
+
+execute dbms_rls.add_policy (object_schema => 'ROLE_GIANGVIEN',
+                            object_name => 'PHANCONG',
+                            policy_name => 'xem_phan_cong_chinh_minh_policy',
+                            function_schema => 'sec_mgr',
+                            policy_function => 'xem_phan_cong_chinh_minh_function',
+                            statement_types => 'select',
+                            update_check => TRUE );
+
+execute dbms_rls.add_policy (object_schema => 'ROLE_GIANGVIEN',
+                            object_name => 'DANGKY',
+                            policy_name => 'xem_lop_chinh_minh_policy',
+                            function_schema => 'sec_mgr',
+                            policy_function => 'xem_phan_cong_chinh_minh_function',
+                            statement_types => 'select',
+                            update_check => TRUE );
+
+execute dbms_rls.add_policy (object_schema => 'ROLE_GIANGVIEN',
+                            object_name => 'DANGKY',
+                            policy_name => 'sua_diem_policy',
+                            function_schema => 'sec_mgr',
+                            policy_function => 'xem_phan_cong_chinh_minh_function',
+                            statement_types => 'update',
+                            sec_relevant_cols => 'DIEMTH', 'DIEMQT', 'DIEMCK', 'DIEMTK');
