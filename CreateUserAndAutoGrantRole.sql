3661735641c81dbd94e43c77a1405058ac873cb8
MageOfFlowers <100255997+MageOfFlowers@users.noreply.github.com>
2024-03-13 16:17:09 +0700

    OLS

--- CreateUserAndAutoGrantRole.sql
+++ CreateUserAndAutoGrantRole.sql
@@ -26,19 +26,30 @@ END;
 
 CREATE OR REPLACE PROCEDURE USP_ADDUSRMEM
 AS
-    CURSOR CUR IS (SELECT MANV, VAITRO
-                    FROM  ADMIN_OLS1.NHANSU
-                    WHERE MANV IN (SELECT USERNAME
+    CURSOR CUR IS (SELECT ns.MANV, ns.VAITRO, dv.TRGDV, dv.TENDV
+                    FROM  ADMIN_OLS1.NHANSU ns inner join ADMIN_OLS1.DONVI dv on ns.MADV = dv.MADV 
+                    WHERE ns.MANV IN (SELECT USERNAME
                                         FROM ALL_USERS));
     STRSQL VARCHAR(2000);
     USR VARCHAR2(5);
+    USR2 VARCHAR2(50);
+    USR3 VARCHAR2(5);
+    USR4 VARCHAR2(50);
 BEGIN
     OPEN CUR;
     LOOP
-        FETCH CUR INTO USR;
+        FETCH CUR INTO USR, USR2;
         EXIT WHEN CUR%NOTFOUND;
-        
-        STRSQL := 'GRANT '||VAITRO||' TO '||USR;
+        IF USR2 = 'Giao vu' THEN
+            STRSQL := 'GRANT GVu TO '||USR;
+        ELSIF USR2 = 'Nhan vien co ban' THEN
+            STRSQL := 'GRANT NV TO '||USR;
+        ELSIF USR3 = USR AND USR4 = 'Van phong khoa' THEN
+            STRSQL := 'GRANT TK TO '||USR;
+        ELSIF USR3 = USR THEN
+            STRSQL := 'GRANT TDV TO '||USR; 
+        ELSE STRSQL := 'GRANT GVien TO '||USR;
+        END IF;
         EXECUTE IMMEDIATE (STRSQL);
     END LOOP;
     CLOSE CUR;
--- OLSab.sql
+++ OLSab.sql
@@ -6,10 +6,10 @@ end;
 
 CREATE OR REPLACE PROCEDURE USP_Cap_quyen_hang_loat_a
 AS
-    CURSOR CUR IS (SELECT MANV
-                    FROM  ADMIN_OLS1.NHANSU
-                    WHERE MANV IN (SELECT USERNAME
-                                        FROM ALL_USERS) AND VAITRO = 'TK');
+    CURSOR CUR IS (SELECT ns.MANV
+                    FROM  ADMIN_OLS1.NHANSU ns inner join ADMIN_OLS1.DONVI dv on ns.MANV = dv.TRGDV 
+                    WHERE ns.MANV IN (SELECT USERNAME
+                                        FROM ALL_USERS) AND dv.TENDV = 'Van phong khoa');
     STRSQL VARCHAR(2000);
     USR VARCHAR2(5);
 BEGIN
@@ -18,33 +18,34 @@ BEGIN
         FETCH CUR INTO USR;
         EXIT WHEN CUR%NOTFOUND;
         
-        STRSQL := 'EXEC Cap_quyen_a ('||MANV||')';
+        STRSQL := 'EXEC Cap_quyen_a ('||USR||')';
         EXECUTE IMMEDIATE (STRSQL);
     END LOOP;
     CLOSE CUR;
 END;
 
-CREATE OR REPLACE PROCEDURE Cap_quyen_b (name in varchar2)
+CREATE OR REPLACE PROCEDURE Cap_quyen_b (name in varchar2, donvi in varchar2)
 is
 begin
-    SA_USER_ADMIN.SET_USER_LABELS('region_policy',name,'TDV: :  CS2'); 
+    SA_USER_ADMIN.SET_USER_LABELS('region_policy',name,'TDV :'||donvi||':CS1, CS2'); 
 end;
 
-CREATE OR REPLACE PROCEDURE USP_Cap_quyen_hang_loat_a
+CREATE OR REPLACE PROCEDURE USP_Cap_quyen_hang_loat_b
 AS
-    CURSOR CUR IS (SELECT MANV
-                    FROM  ADMIN_OLS1.NHANSU
-                    WHERE MANV IN (SELECT USERNAME
-                                        FROM ALL_USERS) AND VAITRO = 'TK');
+    CURSOR CUR IS (SELECT ns.MANV, ns.MADV
+                    FROM  ADMIN_OLS1.NHANSU ns inner join ADMIN_OLS1.DONVI dv on ns.MANV = dv.TRGDV 
+                    WHERE ns.MANV IN (SELECT USERNAME
+                                        FROM ALL_USERS) AND dv.TENDV != 'Van phong khoa');
     STRSQL VARCHAR(2000);
     USR VARCHAR2(5);
+    USR2 VARCHAR2(5);
 BEGIN
     OPEN CUR;
     LOOP
-        FETCH CUR INTO USR;
+        FETCH CUR INTO USR, USR2;
         EXIT WHEN CUR%NOTFOUND;
         
-        STRSQL := 'EXEC Cap_quyen_a ('||MANV||')';
+        STRSQL := 'EXEC Cap_quyen_b ('||USR||','||USR2||')';
         EXECUTE IMMEDIATE (STRSQL);
     END LOOP;
     CLOSE CUR;
