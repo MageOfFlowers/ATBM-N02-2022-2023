131abf991c00904548880040c3550be317b57865
T4loN2110 <anhtainguyenduc@gmail.com>
2024-03-25 19:11:25 +0700

    .

---
+++ ATBM-Nhom02-Phanhe1/ATBM_PhanHe1/ATBM/DTO/NhanSuDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATBM.DTO
+{
+    public class NhanSuDTO
+    {
+        public string MANV { get; set; }
+        public string HOTEN { get; set; }
+        public string PHAI { get; set; }
+        public DateTime NGSINH { get; set; }
+        public double PHUCAP { get; set; }
+        public string DT { get; set; }
+        public string VAITRO { get; set; }
+        public string MADV { get; set; }
+    }
+}
