549264519aa015847d6e77105c9bfed2507c9027
T4loN2110 <anhtainguyenduc@gmail.com>
2024-04-11 22:29:04 +0700

    .

--- CS1.sql
+++ CS1.sql
@@ -1,31 +1,26 @@
-alter session set "_ORACLE_SCRIPT"=true;
-
-create role ROLE_NHANVIEN
-not identified;
-
-create function xem_thong_tin_chinh_minh_function(p_schema varchar2, p_obj varchar2)
+grant select, update(DT) on nhansu to role_nhanvien;
+grant update on nhansu to role_nhanvien;
+grant select on sinhvien to role_nhanvien;
+grant select on donvi to role_nhanvien;
+grant select on hocphan to role_nhanvien;
+grant select on khmo to role_nhanvien;
+/
+create or replace function ADMIN_OLS1.xemBT_nhansu(p_schema varchar2, p_obj varchar2)
 return varchar2
 as
-user varchar2(100);
+user varchar2(1000);
 begin
-user := sys_context('userenv', 'session_user');
-return 'MANV = ' || user;
+    user := 'sys_context(''userenv'', ''session_user'')';
+    return 'MANV = '|| user;
 end;
+/
+execute dbms_rls.add_policy (object_schema => 'ADMIN_OLS1', object_name => 'nhansu', policy_name => 'xemBT_nhansu', function_schema => 'ADMIN_OLS1', policy_function => 'xemBT_nhansu', statement_types => 'select', update_check => TRUE );
 
-execute dbms_rls.add_policy (object_schema => 'ROLE_NHANVIEN',
-                            object_name => 'NHANSU',
-                            policy_name => 'xem_thong_tin_chinh_minh_policy',
-                            function_schema => 'sec_mgr',
-                            policy_function => 'xem_thong_tin_chinh_minh_function',
-                            statement_types => 'select',
-                            update_check => TRUE );
-
-execute dbms_rls.add_policy (object_schema => 'ROLE_NHANVIEN',
-                            object_name => 'SINHVIEN',
-                            policy_name => 'sua_thong_tin_chinh_minh_policy',
-                            function_schema => 'sec_mgr',
-                            policy_function => 'xem_thong_tin_chinh_minh_function',
-                            statement_types => 'update',
-                            sec_relevant_cols => 'ĐT');
-
-grant select on SINHVIEN, DONVI, HOCPHAN, KHMO to ROLE_NHANVIEN
+/*
+execute dbms_rls.drop_policy (object_schema => 'ADMIN_OLS1', object_name => 'nhansu', policy_name => 'xemBT_nhansu')
+*/
+/
+execute dbms_rls.add_policy (object_schema => 'ADMIN_OLS1', object_name => 'nhansu', policy_name => 'suaBT_nhansu', function_schema => 'ADMIN_OLS1', policy_function => 'xemBT_nhansu', statement_types => 'update', sec_relevant_cols => 'DT');
+/*
+execute dbms_rls.drop_policy (object_schema => 'ADMIN_OLS1', object_name => 'nhansu', policy_name => 'suaBT_nhansu')
+*/
--- CS2.sql
+++ CS2.sql
@@ -1,39 +1,34 @@
-alter session set "_ORACLE_SCRIPT"=true;
-
-create role ROLE_GIANGVIEN
-not identified;
-
-grant ROLE_NHANVIE to ROLE_GIANGVIEN;
-
-create function xem_phan_cong_chinh_minh_function(p_schema varchar2, p_obj varchar2)
+grant role_nhanvien to role_giangvien;
+grant select on phancong to role_giangvien;
+grant select, update(diemth, diemqt, diemck, diemtk) on dangky to role_giangvien;
+/
+create or replace function xemBT_phancong(p_schema varchar2, p_obj varchar2)
 return varchar2
 as
 user varchar2(100);
 begin
-user := sys_context('userenv', 'session_user');
+user := 'sys_context(''userenv'', ''session_user'')';
 return 'MAGV = ' || user;
 end;
-
-execute dbms_rls.add_policy (object_schema => 'ROLE_GIANGVIEN',
-                            object_name => 'PHANCONG',
-                            policy_name => 'xem_phan_cong_chinh_minh_policy',
-                            function_schema => 'sec_mgr',
-                            policy_function => 'xem_phan_cong_chinh_minh_function',
-                            statement_types => 'select',
-                            update_check => TRUE );
-
-execute dbms_rls.add_policy (object_schema => 'ROLE_GIANGVIEN',
-                            object_name => 'DANGKY',
-                            policy_name => 'xem_lop_chinh_minh_policy',
-                            function_schema => 'sec_mgr',
-                            policy_function => 'xem_phan_cong_chinh_minh_function',
-                            statement_types => 'select',
-                            update_check => TRUE );
-
-execute dbms_rls.add_policy (object_schema => 'ROLE_GIANGVIEN',
-                            object_name => 'DANGKY',
-                            policy_name => 'sua_diem_policy',
-                            function_schema => 'sec_mgr',
-                            policy_function => 'xem_phan_cong_chinh_minh_function',
-                            statement_types => 'update',
-                            sec_relevant_cols => 'DIEMTH', 'DIEMQT', 'DIEMCK', 'DIEMTK');
+/
+execute dbms_rls.add_policy (object_schema => 'ADMIN_OLS1', object_name => 'phancong', policy_name => 'xemBT_phancong', function_schema => 'ADMIN_OLS1', policy_function => 'xemBT_phancong', statement_types => 'select', update_check => TRUE );
+/
+/*
+execute dbms_rls.drop_policy (object_schema => 'ADMIN_OLS1', object_name => 'phancong', policy_name => 'xemBT_phancong')
+*/
+create or replace function xemBT_dangky(p_schema varchar2, p_obj varchar2)
+return varchar2
+as
+begin
+    return 'MAHP in (select mahp from phancong where magv = sys_context(''userenv'', ''session_user''))';
+end;
+/
+execute dbms_rls.add_policy (object_schema => 'ADMIN_OLS1', object_name => 'dangky', policy_name => 'xemBT_dangky', function_schema => 'ADMIN_OLS1', policy_function => 'xemBT_dangky', statement_types => 'select', update_check => TRUE );
+/
+/*
+execute dbms_rls.drop_policy (object_schema => 'ADMIN_OLS1', object_name => 'dangky', policy_name => 'xemBT_dangky')
+*/
+execute dbms_rls.add_policy (object_schema => 'ADMIN_OLS1', object_name => 'dangky', policy_name => 'suaDiem_dangky', function_schema => 'ADMIN_OLS1', policy_function => 'xemBT_dangky', statement_types => 'update', sec_relevant_cols => 'diemth, diemqt, diemck, diemtk');
+/*
+execute dbms_rls.drop_policy (object_schema => 'ADMIN_OLS1', object_name => 'dangky', policy_name => 'suaDiem_dangky')
+*/
