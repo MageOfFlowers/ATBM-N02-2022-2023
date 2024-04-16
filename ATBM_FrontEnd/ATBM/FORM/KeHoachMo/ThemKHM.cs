27117aa8d26b2026e6d4269c068e5f649dfd8315
T4loN2110 <anhtainguyenduc@gmail.com>
2024-04-16 20:16:32 +0700

    Xong KeHoachMo

--- ATBM_FrontEnd/ATBM/FORM/KeHoachMo/ThemKHM.cs
+++ ATBM_FrontEnd/ATBM/FORM/KeHoachMo/ThemKHM.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,18 @@ namespace ATBM.FORM.KeHoachMo
             loadHP();
         }
 
+        public ThemKHM(KHMDTO khm)
+        {
+            InitializeComponent();
+            loadHP();
+            Them_btn.Text = "Cập nhật";
+            HocPhan_cb.SelectedValue= khm.MAHP;
+            HocPhan_cb.Enabled = false;
+            HocKy_cb.SelectedItem = khm.HOCKY.ToString();
+            NamHoc_cb.SelectedItem = khm.NAM.ToString();
+            ChuongTrinh_cb.SelectedItem = khm.MACT;
+        }
+
         private void ThemKHM_Load(object sender, EventArgs e)
         {
             
@@ -30,6 +43,7 @@ namespace ATBM.FORM.KeHoachMo
         private void loadHP()
         {
             IList<HocPhanDTO> ds_hp = hp.dsHocPhan();
+            ds_hp.Insert(0, new HocPhanDTO());
             HocPhan_cb.DataSource = ds_hp;
             HocPhan_cb.DisplayMember = "TENHP";
             HocPhan_cb.ValueMember = "MAHP";
@@ -40,24 +54,27 @@ namespace ATBM.FORM.KeHoachMo
             KHMDTO k = new KHMDTO();
             k.MAHP = HocPhan_cb.SelectedValue.ToString();
             k.TENHP = HocPhan_cb.Text;
-            MessageBox.Show(k.TENHP);
             k.HOCKY = Convert.ToInt32(HocKy_cb.SelectedItem);
             k.NAM = Convert.ToInt32(NamHoc_cb.SelectedItem);
-            k.MACT = ChuongTrinh_cb.SelectedItem.ToString();
+            k.MACT = ChuongTrinh_cb.Text;
 
-            try
+            if (k.MAHP != null && k.TENHP != null && k.HOCKY != 0 && k.NAM != 0 && k.MACT != null)
             {
-                khm.themKHM(k);
-                MessageBox.Show("Success");
+                try
+                {
+                    khm.themKHM(k);
+                    MessageBox.Show("Thành công");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    Close();
+                }
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                Close();
-            }
+            else MessageBox.Show("Vui lòng điền đầy đủ thông tin");
         }
     }
 }
