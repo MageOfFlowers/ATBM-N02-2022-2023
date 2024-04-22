13eb78af5534c8a2991ee35ebcb46a5f198be9a8
T4loN2110 <anhtainguyenduc@gmail.com>
2024-04-22 10:10:41 +0700

    .

--- Yeu Cau 3 (Audit)/Audit_2a.sql
+++ Yeu Cau 3 (Audit)/Audit_2a.sql
@@ -1,15 +1,26 @@
 GRANT create session, audit_admin, audit_viewer TO admin_ols1;
 /
+CREATE OR REPLACE FUNCTION lay_ds_giang_vien
+RETURN NUMBER
+IS
+  emp_count NUMBER;
+BEGIN
+  SELECT COUNT(*) INTO emp_count
+  FROM EMPLOYEES
+  WHERE DEPARTMENT_ID = dept_id;
+
+  RETURN emp_count;
+END;
+/
 BEGIN
   DBMS_FGA.ADD_POLICY(
     object_schema   => 'ADMIN_OLS1',
     object_name     => 'DANGKY',
     policy_name     => 'AUDIT_2A',
-    audit_condition => 'SYS_CONTEXT(''USERENV'', ''SESSION_USER'') NOT IN (SELECT MANV FROM ADMIN_OLS1.NHANSU WHERE VAITRO = ''Giang vien'')',
+    audit_condition => '(select granted_role from dba_role_privs where grantee = SYS_CONTEXT(''USERENV'', ''SESSION_USER'')) != ''ROLE_GIANGVIEN''',
     audit_column    => 'DIEMTH, DIEMQT, DIEMCK, DIEMTK',        
-    statement_types => 'UPDATE',
-    handler_module  => NULL,
-    enable          => TRUE
+    enable          => TRUE,
+    statement_types => 'UPDATE'
   );
 END;
 /
@@ -22,8 +33,8 @@ BEGIN
   );
 END;
 */
-
+select granted_role from dba_role_privs where grantee = SYS_CONTEXT('USERENV', 'SESSION_USER')
 SELECT * FROM DBA_FGA_AUDIT_TRAIL
 WHERE policy_name = 'AUDIT_2A';
-
+grant select on nhansu to role_giangvien;
 update DANGKY set diemth = 10, diemtk = 5 where masv = 'SV002';
