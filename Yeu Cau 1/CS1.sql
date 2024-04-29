20c59c3d4cc8b3743c4a383041377cf9b83298a0
T4loN2110 <anhtainguyenduc@gmail.com>
2024-04-29 21:46:44 +0700

    .

--- Yeu Cau 1/CS1.sql
+++ Yeu Cau 1/CS1.sql
@@ -9,16 +9,38 @@ revoke role_nhanvien from admin_ols1;
 create or replace function ADMIN_OLS1.xemBT_nhansu(p_schema varchar2, p_obj varchar2)
 return varchar2
 as
-user varchar2(1000);
+m_user varchar2(100);
+m_role varchar2(100);
 begin
-    user := 'sys_context(''userenv'', ''session_user'')';
-    return 'MANV = '|| user;
+    m_user := sys_context('userenv', 'session_user');
+    select vaitro into m_role
+    from admin_ols1.nhansu
+    where manv = m_user;
+    exception
+    when NO_DATA_FOUND then
+        m_role := 'Unknown';
+    if (m_role = 'Nhan vien co ban') then
+        return 'MANV = '|| m_user;
+    else 
+        return '1=1';
+    end if;
 end;
 /
-execute dbms_rls.add_policy (object_schema => 'ADMIN_OLS1', object_name => 'nhansu', policy_name => 'xemBT_nhansu', function_schema => 'ADMIN_OLS1', policy_function => 'xemBT_nhansu', statement_types => 'select', update_check => TRUE );
-
+begin 
+    dbms_rls.add_policy (object_schema => 'ADMIN_OLS1', 
+                         object_name => 'nhansu', 
+                         policy_name => 'xemBT_nhansu', 
+                         function_schema => 'ADMIN_OLS1',
+                         policy_function => 'xemBT_nhansu',
+                         statement_types => 'select',
+                         update_check => TRUE );
+end;
 /*
-execute dbms_rls.drop_policy (object_schema => 'ADMIN_OLS1', object_name => 'nhansu', policy_name => 'xemBT_nhansu')
+begin
+    dbms_rls.drop_policy (object_schema => 'ADMIN_OLS1', 
+                          object_name => 'nhansu', 
+                          policy_name => 'xemBT_nhansu');
+end;
 */
 /
 execute dbms_rls.add_policy (object_schema => 'ADMIN_OLS1', object_name => 'nhansu', policy_name => 'suaBT_nhansu', function_schema => 'ADMIN_OLS1', policy_function => 'xemBT_nhansu', statement_types => 'update', sec_relevant_cols => 'DT');
