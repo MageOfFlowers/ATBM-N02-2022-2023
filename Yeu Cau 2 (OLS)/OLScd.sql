0fb8d0883b75569c209cbddc3c8e914addd5b7ad
Bui Nguyen Tin <buinguyentin28062003@gmail.com>
2024-05-01 17:08:36 +0700

    Update OLScd.sql

--- Yeu Cau 2 (OLS)/OLScd.sql
+++ Yeu Cau 2 (OLS)/OLScd.sql
@@ -1,15 +1,18 @@
 --c) Gan nhan cho 1 giáo v? ??c toan bo thong bao danh cho giao vu
-CREATE OR REPLACE PROCEDURE Cap_quyen_a (name in varchar2)
+CREATE OR REPLACE PROCEDURE Cap_quyen_c (name in varchar2)
 is
 begin
-    SA_USER_ADMIN.SET_USER_LABELS('region_policy2',name,'TK:HTTT,CNPM,KHMT,CNTT,TGMT,MMT:CS1,CS2'); 
+    SA_USER_ADMIN.SET_USER_LABELS('region_policy2',name,'GVU:HTTT,CNPM,KHMT,CNTT,TGMT,MMT:CS1,CS2'); 
 end;
 
+//exec Cap_quyen_c('NV006');
+
+
 --d)
-create or replace procedure gan_nhan_g
+create or replace procedure gan_nhan_d
 is
 begin
 update ADMIN_OLS1.THONGBAO
-set region_label2 = char_to_label('region_policy2','TK:KHMT:CS1, CS2')
+set region_label2 = char_to_label('region_policy2','TDV::')
 where id = 4;
 end;
